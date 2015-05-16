84a5801d37dfe20fcd6ace845f068ac1424acc3d
Vishal Sharma <vishal2033@gmail.com>
2015-05-16 13:19:08 +0530

    ES 1st UI working integration

--- app6/public/index.js
+++ app6/public/index.js
@@ -6,20 +6,68 @@ function onAppReady(){
 
 function AntlrApp(){
 	this.init();
+	this.initUI();
+}
+
+AntlrApp.prototype.initUI = function(){
+	$('#results').text('');
+	$('.resultsTable tbody').html('');
+	$('.searchInput').val('show apple in north where state=pun and model=iphone5 and date >= 2010/01/1 and date <= 2012-1-1');
+
 }
 
 AntlrApp.prototype.init = function(){
-	$('#run').on('click', this.getData.bind(this));
+	this.initUI();
+	$('#run').on('click', this.executeQuery.bind(this));
+	$(document).on('keypress', this.handleKeyPressEvent.bind(this));
 }
 
-AntlrApp.prototype.getData = function(e){
-	var query = 'show apple in north where state=pun and model=iphone5 and date >= 2010/01/1 and date <= 2012-1-1';
+AntlrApp.prototype.executeQuery = function(e){
+	var query = $('.searchInput').val();
 	query = encodeURIComponent(query);
 	var url = '/data?q=' + query;
 
 	$.getJSON(url, this.onDataReceived.bind(this));
 }
 
-AntlrApp.prototype.onDataReceived = function(data){
-	console.log(data);
+AntlrApp.prototype.handleKeyPressEvent = function(e){
+	if(e.keyCode !== 13) return;
+
+	this.executeQuery();
 }
+
+AntlrApp.prototype.onDataReceived = function(data){
+	if(data.success){
+		this.showResultsTable(data.results);
+	}
+	else
+		alert('Internal server error');
+}
+
+AntlrApp.prototype.showResultsTable = function(resp){
+	var total = resp.hits.total;
+	var hits = resp.hits.hits;
+	var count = hits.length;
+	var time = resp.took;
+	$('#results').text('Showing ' + count +' of ' + total + ' results in ' + time + ' milliseconds');
+
+	var rowHtml = '<tr>'+
+			        '<td align="center">ID_PRODUCT</td>'+
+			    	'<td align="center">ID_REGION</td>'+
+			    	'<td align="center">ID_CUSTOMER</td>'+
+			  	  '</tr>';
+	var $tbody = $('.resultsTable tbody');
+	hits.forEach(function(h){
+		var p = h._source.product;
+		var product = p.category + '-' + p.type + '-' + p.brand + '-' + p.model;
+		var r = h._source.region;
+		var region = r.city + '-' + r.state + ' (' + r.region + ')';
+		var customer = h._source.customer.name + '-' + h._source.customer.id;
+
+		var row = rowHtml.replace('ID_PRODUCT', product).replace('ID_REGION', region).replace('ID_CUSTOMER', customer);
+		var $trow = $(row);
+		$trow.appendTo($tbody);
+	});
+
+}
+
