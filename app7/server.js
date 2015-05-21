f526b319314c838ddae0a04269692603077aa37f
Vishal Sharma <vishal2033@gmail.com>
2015-05-21 11:45:09 +0530

    app level exception handling & logging

--- app7/server.js
+++ app7/server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = new express();
 var apiRouter = require('./server/routers/ApiRouter');
+var logger = require('./server/utils/Logger');
 
 app.use(express.static('web'));
 app.use('/api', apiRouter);
+
+app.use(function(err, req, res, next) {
+  logger.log(err.stack);
+  res.status(500).json({success:false, data:'internal server error'});
+});
 
 var port = 9090;
 app.listen(port);
