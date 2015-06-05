f22b09fad69286f579c1e2cfb5d5c99af53ae113
Vishal Sharma <vishal2033@gmail.com>
2015-06-05 11:57:55 +0530

    2 pane views for category brand distribution

--- app7/web/v1/categoryBrands.js
+++ app7/web/v1/categoryBrands.js
@@ -1,4 +1,26 @@
 function CategoryBrands(){
-
+	this.init();
 }
+
+CategoryBrands.prototype.init = function(){
+	this.addEventHandlers();
+}
+
+CategoryBrands.prototype.addEventHandlers = function(){
+	$('#category').on('change', this.onCategoryChange.bind(this));
+}
+
+CategoryBrands.prototype.onCategoryChange = function(){
+	$('.bElecContainer').removeClass('bActive');
+	$('.bApplianceContainer').removeClass('bActive');
+	$('.bClothsContainer').removeClass('bActive');
+	$('.bAutocontainer').removeClass('bActive');
+	switch($('#category').val()){
+		case 'Automobiles' : $('.bAutocontainer').addClass('bActive'); break;
+		case 'Electronics' : $('.bElecContainer').addClass('bActive'); break;
+		case 'Appliances' : $('.bApplianceContainer').addClass('bActive'); break;
+		case 'Cloths' : $('.bClothsContainer').addClass('bActive'); break;
+	}
+}
+
 
