617c2fb0127c7d416b49fd61475e6cd24021254d
Vishal Sharma <vishal2033@gmail.com>
2015-06-10 13:50:28 +0530

    x axis year lables complete

--- app7/web/v2/src/SalesAppController.js
+++ app7/web/v2/src/SalesAppController.js
@@ -16,47 +16,8 @@ SalesAppController.prototype.onApiResponse = function(resp){
 	var salesTimeModel = new SalesTimeModel(options);
 	var catSalesInTime = salesTimeModel.getCategorySalesInTime(resp.results.aggregations.categories.buckets);
 
-	options.frmStartX = 20;
-	options.frmStartY = 250;
+	options.frmStartX = 30;
+	options.frmStartY = 225;
 	var salesTimeView = new SalesTimeView();
 	salesTimeView.renderCategorySalesInTime(catSalesInTime, options);
-return;
-
-	var svg = d3.selectAll('svg');
-
-	var g = svg.append('g')
-		       .attr('transform', 'translate(50, 250) scale(1, -1)');
-
-	
-	var yearlySales = resp.results.aggregations.yearly.buckets;
-	var min = d3.min(yearlySales, function(y){
-		return y.doc_count;
-	});
-	var max = d3.max(yearlySales, function(y){
-		return y.doc_count;
-	});
-	var xScale = d3.scale.linear()
-						  .domain([2000, 2014])
-						  .range([0, 400]);
-	var yScale = d3.scale.linear()
-						  .domain([min, max])
-						  .range([30, 200]);
-	var dataset = [];
-	var dStart = 0;
-	var w = 60;
-	yearlySales.forEach(function(y){
-		dataset.push([dStart, yScale(y.doc_count)]);
-		dStart+=w;
-	});
-	g.selectAll('rect')
-	 .data(dataset)
-	 .enter()
-	 .append('rect')
-	 .attr({
-	 	x : function(d) { return d[0]; },
-	 	y : 30,
-	 	width : w,
-	 	height : function(d) { return d[1]; },
-	 	style : "stroke-width : 1px; stroke : white; fill: none"
-	 });
 }
--- app7/web/v2/src/views/SalesTimeView.js
+++ app7/web/v2/src/views/SalesTimeView.js
@@ -3,14 +3,15 @@ function SalesTimeView(){
 }
 
 SalesTimeView.prototype.renderCategorySalesInTime = function(catSalesInTime, options){
+	var yBlockWd = this.yearBlockWidth(catSalesInTime);
 	var svg = d3.selectAll('svg');
 
 	var g = svg.append('g')
 		       .attr('transform', 'translate(' + options.frmStartX + ',' + options.frmStartY + ') scale(1, -1)');
-
-    Object.keys(catSalesInTime).forEach(function(yKey){
+    var xEnd = 0;	       
+    Object.keys(catSalesInTime).forEach((function(yKey){
     	var catSaleInYear = catSalesInTime[yKey];
-    	Object.keys(catSaleInYear).forEach(function(cKey){
+    	Object.keys(catSaleInYear).forEach((function(cKey){
     		var c = catSaleInYear[cKey];
     		g.append('rect')
 			 .attr({
@@ -18,8 +19,58 @@ SalesTimeView.prototype.renderCategorySalesInTime = function(catSalesInTime, opt
 			 	y : c.y,
 			 	width : c.W,
 			 	height : c.H,
-			 	style : "stroke-width : 1px; stroke : white; fill: none"
+			 	style : this.getFillStyleForCategory(cKey)
 			 });
-    	});
-    });
+			 xEnd = c.x + c.W;
+    	}).bind(this));
+    	this.addLine(g, xEnd, 0, xEnd, -6);          // x axis ticks
+    	this.addText(g, (xEnd-yBlockWd/2), 15, yKey);
+    }).bind(this));
+
+    this.addLine(g, 0, 0, xEnd, 0);                  // x axis
+    this.addLine(g, 0, 0, 0, options.frmHeight);	 // y axis
+}
+
+SalesTimeView.prototype.getFillStyleForCategory = function(category){
+	var style = '';
+	switch(category){
+		case 'Automobile' : style = 'stroke-width : 1px; stroke : white; fill: #2b908f'; break;
+		case 'Electronics' : style = 'stroke-width : 1px; stroke : white; fill: #90ee7e'; break;
+		case 'Applicance' : style = 'stroke-width : 1px; stroke : white; fill: #f45b5b'; break;
+		case 'Clothing' : style = 'stroke-width : 1px; stroke : white; fill: #7798BF'; break;
+	}
+	return style;
+}
+
+SalesTimeView.prototype.addLine = function(g, px1, py1, px2, py2, s){
+	g.append('line')
+	 .attr({
+		x1 : px1 || 0,
+		y1 : py1 || 0,
+		x2 : px2 || 0,
+		y2 : py2 || 0,
+		style : s || 'stroke-width : 1px; stroke : grey;'
+	});
+}
+
+SalesTimeView.prototype.addText = function(g, x, y, label){
+	g.append('g')
+	 .attr('transform', 'scale(1,-1)')
+	 .append('text')
+	 .attr({
+	 	x : x || 0,
+	 	y : y || 0,
+	 	'text-anchor' : 'middle',
+	 	style:'color:grey;cursor:default;font-size:11px;fill:grey;'
+	 })
+	 .text(label);
+}
+
+SalesTimeView.prototype.yearBlockWidth = function(catSalesInTime){
+	var w = 0;
+	var y1 = Object.keys(catSalesInTime)[0];
+	Object.keys(catSalesInTime[y1]).forEach(function(c){
+		w += (2 * catSalesInTime[y1][c].W);
+	});
+	return w;
 }
