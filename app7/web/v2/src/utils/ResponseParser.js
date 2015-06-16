238cccb686765409b5a46f185e42d7d3a18f8cf1
Vishal Sharma <vishal2033@gmail.com>
2015-06-16 21:19:45 +0530

    response parser WIP

--- app7/web/v2/src/utils/ResponseParser.js
+++ app7/web/v2/src/utils/ResponseParser.js
@@ -1,12 +1,96 @@
 function ResponseParser (){
 
 }
+var sc = {
+	category : 1,
+	category_in_region : 2,
+	category_in_state : 3,
+	category_in_city : 4,
+	type : 5,
+	type_in_region : 6,
+	type_in_state : 7,
+	type_in_city : 8,
+	brand : 9,
+	brand_in_region : 10,
+	brand_in_state : 11,
+	brand_in_city : 12,
+	model : 13,
+	model_in_region : 14,
+	model_in_state : 15,
+	model_in_city : 16,
+	region : 17,
+	state : 18,
+	city : 19
+};
 
 ResponseParser.prototype.parse = function(apiRes){
 	console.log(apiRes);
 	this.query = apiRes.query.query;
+	this.context = apiRes.query.searchContext;
 	this.filters = apiRes.query.filters ? apiRes.query.filters.and.concat(apiRes.query.filters.or) : null;
 	this.init();
+	var uiObject = {};
+	switch(this.context){
+		case sc.category :
+			uiObject = this.getCategoryUIObj();
+			break;
+		case sc.category_in_region :
+			uiObject = this.getCategoryInRegionUIObj();
+			break;
+		case sc.category_in_state :
+			uiObject = this.getCategoryInStateUIObj();
+			break;
+		case sc.category_in_city :
+			uiObject = this.getCategoryInCityUIObj();
+			break;
+		case sc.type :
+			uiObject = this.getTypeUIObj();
+			break;
+		case sc.type_in_region :
+			uiObject = this.getTypeInRegionUIObj();
+			break;
+		case sc.type_in_state :
+			uiObject = this.getTypeInStateUIObj();
+			break;
+		case sc.type_in_city :
+			uiObject = this.getTypeInCityUIObj();
+			break;
+		case sc.brand :
+			uiObject = this.getBrandUIObj();
+			break;
+		case sc.brand_in_region :
+			uiObject = this.getBrandInRegionUIObj();
+			break;
+		case sc.brand_in_state :
+			uiObject = this.getBrandInStateUIObj();
+			break;
+		case sc.brand_in_city :
+			uiObject = this.getBrandInCityUIObj();
+			break;
+		case sc.model :
+			uiObject = this.getModelUIObj();
+			break;
+		case sc.model_in_region :
+			uiObject = this.getModelInRegionUIObj();
+			break;
+		case sc.model_in_state :
+			uiObject = this.getModelInStateUIObj();
+			break;
+		case sc.model_in_city :
+			uiObject = this.getModelInCityUIObj();
+			break;
+		case sc.region :
+			uiObject = this.getRegionUIObj();
+			break;
+		case sc.state :
+			uiObject = this.getStateUIObj();
+			break;
+		case sc.city :
+			uiObject = this.getCityUIObj();
+			break;		
+	}
+	this.addTimeAggregate(agg);
+	return agg;
 
 }
 
@@ -78,4 +162,61 @@ ResponseParser.prototype.getUIKeyTemplate = function(){
 		timeSeries : {},
 		product : {}
 	}
+}
+
+ResponseParser.prototype.getCategoryUIObj = function(){
+}
+
+ResponseParser.prototype.getCategoryInRegionUIObj = function(){
+}
+
+ResponseParser.prototype.getCategoryInStateUIObj = function(){
+}
+
+ResponseParser.prototype.getCategoryInCityUIObj = function(){
+}
+
+ResponseParser.prototype.getTypeUIObj = function(){
+}
+
+ResponseParser.prototype.getTypeInRegionUIObj = function(){
+}
+
+ResponseParser.prototype.getTypeInStateUIObj = function(){
+}
+
+ResponseParser.prototype.getTypeInCityUIObj = function(){
+}
+
+ResponseParser.prototype.getBrandUIObj = function(){
+}
+
+ResponseParser.prototype.getBrandInRegionUIObj = function(){
+}
+
+ResponseParser.prototype.getBrandInStateUIObj = function(){
+}
+
+ResponseParser.prototype.getBrandInCityUIObj = function(){
+}
+
+ResponseParser.prototype.getModelUIObj = function(){
+}
+
+ResponseParser.prototype.getModelInRegionUIObj = function(){
+}
+
+ResponseParser.prototype.getModelInStateUIObj = function(){
+}
+
+ResponseParser.prototype.getModelInCityUIObj = function(){
+}
+
+ResponseParser.prototype.getRegionUIObj = function(){
+}
+
+ResponseParser.prototype.getStateUIObj = function(){
+}
+
+ResponseParser.prototype.getCityUIObj = function(){
 }
