848ef5cda3f32dbf37bf81b830adf82920b9456c
Vishal Sharma <vishal2033@gmail.com>
2015-06-17 12:51:03 +0530

    Response parser rewrite with recurssion

--- app7/web/v2/src/utils/ResponseParser.js
+++ app7/web/v2/src/utils/ResponseParser.js
@@ -3,37 +3,44 @@ function ResponseParser (){
 }
 
 ResponseParser.prototype.parse = function(apiRes){
-	var uiObject = {};
-	var agg = apiRes.results[0].aggregations;
-	function addHierarchy (obj, apiObj, strRootKey){
-		obj.key = strRootKey;
-		obj.items = [];
-		if(apiObj.buckets){
-			apiObj.buckets.forEach(function(bucket){
-				var bKeys = Object.keys(bucket);
-				var item = { key : '', items : [] };	
-				bKeys.forEach(function(bKey){													
-					if(typeof(bucket[bKey]) === 'object'){
-						var t = {key : ''};
-						item.items.push(t);
-						addHierarchy(t, bucket[bKey], bKey);
-					}
-					else
-						item[bKey] = bucket[bKey];				
-				});	
-				obj.items.push(item);			
-			});
-		}
-	}
-
-	var i = 1;
-	Object.keys(agg).forEach(function(a){
-		var k = 'key' + i;
-		uiObject[k] = {};
-		addHierarchy(uiObject[k], agg[a], a);
-		i++;
-	});
-	console.log(uiObject);
-	return uiObject;
+	var uiObjects = [];
+	apiRes.results.forEach((function(result){
+		uiObjects.push(this.getUIObjectForAggregation(result.aggregations));
+	}).bind(this));
+	console.log(uiObjects);
+	return uiObjects;
 }
 
+ResponseParser.prototype.getUIObjectForAggregation = function(agg){
+	var uiObject = {};
+	var i = 1;
+	Object.keys(agg).forEach((function(a){
+		var k = 'key' + i;
+		uiObject[k] = {};
+		this.addNodesRecurssive(uiObject[k], agg[a], a);
+		i++;
+	}).bind(this));
+	return uiObject;	
+}
+
+ResponseParser.prototype.addNodesRecurssive = function(obj, apiObj, strRootKey){
+	var self = this;
+	obj.key = strRootKey;
+	obj.items = [];
+	if(apiObj.buckets){
+		apiObj.buckets.forEach(function(bucket){
+			var bKeys = Object.keys(bucket);
+			var item = { key : '', items : [] };	
+			bKeys.forEach(function(bKey){													
+				if(typeof(bucket[bKey]) === 'object'){
+					var t = {key : ''};
+					item.items.push(t);
+					self.addNodesRecurssive(t, bucket[bKey], bKey);
+				}
+				else
+					item[bKey] = bucket[bKey];				
+			});	
+			obj.items.push(item);			
+		});
+	}
+}
