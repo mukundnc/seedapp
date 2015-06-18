5c3876cd99e2b17d2ecf2dcd4881cf8e270ffa57
Vishal Sharma <vishal2033@gmail.com>
2015-06-18 19:06:18 +0530

    sales time group model WIP

--- app7/web/v2/src/models/SalesTimeModel.js
+++ app7/web/v2/src/models/SalesTimeModel.js
@@ -7,7 +7,68 @@ SalesTimeModel.prototype.init = function(options){
 
 }
 
-SalesTimeModel.prototype.getModel = function(uiTimeObj, options){
-	this.init(options);
-	console.log('m here');
+SalesTimeModel.prototype.getModel = function(uiTimeObjs, options){
+	this.init(options);	
+	var times = {
+		key1 : {
+			timeGroups : []
+		},
+		key2 : {
+			timeGroups : []
+		}
+	};
+	uiTimeObjs.forEach((function(uiTimeObj){
+		Object.keys(uiTimeObj).forEach((function(key){
+			if(key === 'key1' || key === 'key2'){
+				times[key].timeGroups = this.getTimeGroups(uiTimeObj[key].items);
+			}
+		}).bind(this));
+	}).bind(this));
+	return times;
+}
+
+SalesTimeModel.prototype.getTimeGroups = function(uiTimeItems){
+	var tKey = this.getTimeGroupKey(uiTimeItems[0].items);
+	var iGroups = this.getItemGroups(uiTimeItems);
+	var timeGroups = [];
+
+	for(var i = 0 ; i < iGroups.length ; i++){
+		var timeGroup = this.getTimeGroup(iGroups[i]);
+		timeGroups.push(timeGroup);
+	}
+	return timeGroups;
+}
+
+SalesTimeModel.prototype.getTimeGroupKey = function(uiTimeItems){
+	var times = ['yearly', 'monthly', 'daily'];
+	for(var i = 0 ; i < uiTimeItems.length; i++){
+		if(times.indexOf(uiTimeItems[i].key) !== -1)
+			return i;
+	}
+}
+
+SalesTimeModel.prototype.getItemGroups = function(uiTimeItems){
+	if(uiTimeItems.length < 5) return [uiTimeItems];
+
+	var bContinue = true;
+	var groups = [];
+	while(bContinue){
+		var group = [];
+		for(i = 0 ; i < 5 ; i++){
+			var t = uiTimeItems.pop();
+			if(t)
+				group.push(t);
+			else{
+				bContinue = false;
+				break;
+			}
+		}
+		if(group.length > 0)
+			groups.push(group);
+	}
+	return groups;
+}
+
+SalesTimeModel.prototype.getTimeGroup = function(uiTimeItems, tKey){
+	console.log(uiTimeItems);
 }
