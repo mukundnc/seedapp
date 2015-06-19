d63fef4e918f456d34321d0a1d942da0d969616b
Vishal Sharma <vishal2033@gmail.com>
2015-06-19 13:00:47 +0530

    sales time group back next completed

--- app7/web/v2/src/views/SalesTimeView.js
+++ app7/web/v2/src/views/SalesTimeView.js
@@ -63,6 +63,7 @@ SalesTimeView.prototype.addTimeGroupLabels = function(tgCount){
 	$('.st-labels').on('click', function(e){
 		self.onTimeLabelChange(this);
 	});
+	this.addTimeGroupBackNext(tgCount);
 }
 
 SalesTimeView.prototype.onTimeLabelChange = function(selTimeLabel){
@@ -70,6 +71,38 @@ SalesTimeView.prototype.onTimeLabelChange = function(selTimeLabel){
 	d3.selectAll('.st-text-select').classed('st-text-select', false).classed('st-text', true);
 	d3.select(selTimeLabel).select('rect').attr('class', 'st-select');
 	d3.select(selTimeLabel).select('text').attr('class', 'st-text-select');
+}
+
+SalesTimeView.prototype.addTimeGroupBackNext = function(tgCount){
+	//if(tgCount < 6) return;
+	var g = this.getTimeLabelsSvgGroup();
+	var xS = this.options.w - 250;
+	var yS = this.options.h - 120;
+	var rH = 15;
+	var rW = 15;
+	var gR = null;
+	var labels = ['<', '>'];
+	var id = 'stBack';
+	labels.forEach((function(label){
+		gR = this.utils.addRectLabel(g, xS, yS, rW, rH, label, 'st-bn', 'st-bn-rect', 'st-bn-text', 'start');
+		gR.attr('id', id);
+		id = 'stNext';
+		xS += rW;
+	}).bind(this));
+	gR.select('rect').classed('st-bn-select', true);
+	gR.select('text').classed('st-bn-text-select', true);
+	var self = this;
+	$('.st-bn').on('click', function(e){
+		self.onTimeGroupBackNext(this);
+	});
+
+}
+
+SalesTimeView.prototype.onTimeGroupBackNext = function(selBackNext){
+	d3.selectAll('.st-bn-select').classed('st-bn-select', false).classed('st-bn-rect', true);
+	d3.selectAll('.st-bn-text-select').classed('st-bn-text-select', false).classed('st-bn-text', true);
+	d3.select(selBackNext).select('rect').attr('class', 'st-bn-select');
+	d3.select(selBackNext).select('text').attr('class', 'st-bn-text-select');
 }
 
 SalesTimeView.prototype.renderTimeGroup = function(timeGroup){
