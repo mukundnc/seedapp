531b8e579ad5d751f542e70d30245982a381e8be
Vishal Sharma <vishal2033@gmail.com>
2015-06-24 16:31:13 +0530

    search container pie view 1st cut

--- app7/web/v3/src/views/SearchFrameView.js
+++ app7/web/v3/src/views/SearchFrameView.js
@@ -44,6 +44,7 @@ SearchFrameView.prototype.renderTabs = function(){
 	var xForm = this.utils.getCodtSystemXForm(xOrg, tabH);
 	var g = this.utils.getGroupByClassName('sf-frame-group');
 	g.attr('transform', xForm);
+	g.html('');
 	var keys = Object.keys(this.tabs);
 	var cssRect = 'sf-rect-select', cssText = 'sf-text-select', i = 0;
 	keys.forEach((function(key){
