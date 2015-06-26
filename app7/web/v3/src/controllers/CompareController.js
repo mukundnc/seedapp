04eacca6ded7076ca0738b5a5b7db0939c1bec82
Vishal Sharma <vishal2033@gmail.com>
2015-06-26 18:34:33 +0530

    compare model WIP

--- app7/web/v3/src/controllers/CompareController.js
+++ app7/web/v3/src/controllers/CompareController.js
@@ -1,6 +1,9 @@
 function CompareController(appController){
 	this.appController = appController;
 	this.qidResults = {};
+	this.modelFactory = new ModelFactory();
+	this.H = $('.svg-container').height();
+	this.W = $('.svg-container').width();
 }
 
 CompareController.prototype.renderView = function(qid, apiRes){
@@ -8,6 +11,86 @@ CompareController.prototype.renderView = function(qid, apiRes){
 		this.qidResults[qid] = apiRes;
 
 	var results = this.qidResults[qid];
+	var models = this.modelFactory.getCompareFrameModel(apiRes, this.getModelOptions(results.query));
+}
 
-	console.log('m here');
+CompareController.prototype.getModelOptions = function(query){
+	var dd = this.getDateDetails(query);
+	return {
+		container : {
+			width : this.W - 50,
+			height : this.H * 0.25
+		},
+		timeline : {			
+			startDate : dd.startDate,
+			endDate : dd.endDate,
+			dateDist : dd.dist,
+			width : this.W + 50,
+			height : this.H * 0.37,
+		}
+	};
+}
+
+CompareController.prototype.getViewOptions = function(resultCount, qid){
+	return {
+		controller : this,
+		resultCount : resultCount,
+		w : this.W,
+		h : this.H,
+		container : {
+			qid : qid,
+			controller : this,
+			xOrg : 0,
+			yOrg : this.H * 0.5,
+			w : this.W - 50,
+			h : this.H * 0.45
+		},
+		timeline : {
+			qid : qid,
+			controller : this,
+			xOrg : 90,
+			yOrg : this.H * 0.9,
+			w : this.W + 50,
+			h : this.H * 0.45
+		}
+	}
+}
+
+CompareController.prototype.getDateDetails = function(query){
+	if(!query.filters){
+		return {
+			startDate : '2000/01/01',
+			endDate : '2014/12/31',
+			dist : 'yearly'
+		}
+	}
+	var arr = [];
+	var filters = query.filters.and.concat(results.query.filters.or);
+	filters.forEach(function(f){
+		if(f.filter.isDate){
+			arr.push(f.filter.value);
+		}
+	});
+
+	if(arr.length < 2)
+		return {
+			startDate : '2000/01/01',
+			endDate : '2014/12/31',
+			dist : 'yearly'
+		}
+
+	var details = {
+		startDate : arr[0],
+		endDate : arr[1],
+		dist : 'yearly'
+	};
+	var diff = new Date(arr[1]) - new Date (arr[0]);
+	var dDays = diff/(1000 * 60 * 60 * 24);
+
+	if(dDays < 61)
+		details.dist = 'daily'
+	else if(dDays < 366)
+		details.dist = 'monthly'
+
+	return details;
 }
--- app7/web/v3/src/models/ModelFactory.js
+++ app7/web/v3/src/models/ModelFactory.js
@@ -80,3 +80,98 @@ ModelFactory.prototype.getContainer = function(table, options){
 	}
 	return container;
 }
+
+ModelFactory.prototype.getCompareFrameModel = function(apiRes, options){	
+	var meta = this.getCompareModelMeta(apiRes, options);
+	var compareFrame = this.getCompareModelTmpl();
+	var regionTypes = ['regions', 'states', 'cities', 'region', 'state', 'city'];
+	var productTypes = ['categories', 'types', 'brands', 'models', 'category', 'type', 'brand', 'model'];
+
+	function isProductType(p){
+		return productTypes.indexOf(p) !== -1;
+	}
+
+	function isRegionType(t){
+		return regionTypes.indexOf(t) !== -1;
+	}
+
+	for(var i = 0 ; i < meta.frames.length; i += meta.tabCount){
+		compareFrame.label = meta.frames[i].label;
+		compareFrame.type = meta.frames[i].type;		
+		if(isProductType(compareFrame.type)){
+			compareFrame.container.sectors.top.push({
+				key : meta.qSources.pop().value,
+				count : meta.frames[i].container.sectors.totalCount	
+			});
+		}
+		else{
+			var fs = meta.frames[i].container.sectors.top.concat(meta.frames[i].container.sectors.others);
+			compareFrame.container.sectors.top = compareFrame.container.sectors.top.concat(fs);
+		}
+	}
+	compareFrame.container.sectors.top = _.sortBy(compareFrame.container.sectors.top, function(d){ return d.count; }).reverse();
+	if(compareFrame.container.sectors.top.length > 5){
+		compareFrame.container.sectors.others = compareFrame.container.sectors.top.splice(5, compareFrame.container.sectors.top.length - 5);
+	}
+	compareFrame.container.sectors.others.forEach(function(o){
+		compareFrame.container.sectors.othersCount += o.count;
+	});
+	compareFrame.container.sectors.totalCount = compareFrame.container.sectors.othersCount;
+	compareFrame.container.sectors.top.forEach(function(t){
+		compareFrame.container.sectors.totalCount += t.count;
+	});
+	console.log(compareFrame);
+	return compareFrame;
+}
+
+ModelFactory.prototype.getCompareModelMeta = function(apiRes, options){
+	var resultsCount = apiRes.results.length;
+	var aRes = { results : [apiRes.results[0]] };
+	var framesFirst = this.getFrameModel(aRes, options);
+	var tabCount = framesFirst.length;
+	aRes.results = apiRes.results.slice(1);
+	var framesRest = this.getFrameModel(aRes, options);
+	var frames = framesFirst.concat(framesRest);
+	console.log(frames);
+	var qSources = [];
+	var qTargets = [];
+	apiRes.results.forEach(function(r){
+		if(r.qSource)
+			qSources.push(r.qSource);
+		if(r.qTarget)
+			qTargets.push(r.qTarget);
+	});
+	qSources.reverse();
+	qTargets.reverse();
+	return {
+		resultsCount : resultsCount,
+		tabCount : tabCount,
+		frames : frames,
+		qSources : qSources,
+		qTargets : qTargets
+	}
+}
+
+ModelFactory.prototype.getCompareModelTmpl = function(){
+	return {
+		type : '',
+		label : '',
+		container : {
+			sectors : {
+				top : [],
+				others : [],
+				totalCount : 0,
+				othersCount : 0,
+				type : ''
+			}
+		},
+		timeline : {
+			axes : {},
+			timeGroups : [],
+			type : ''
+		}
+	}
+}
+
+
+
