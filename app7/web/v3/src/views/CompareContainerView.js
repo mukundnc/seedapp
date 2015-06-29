a61ab8470f31ac41fa3b0d47d42367c93861cc2f
Vishal Sharma <vishal2033@gmail.com>
2015-06-29 19:39:42 +0530

    header sales value in compare container

--- app7/web/v3/src/views/CompareContainerView.js
+++ app7/web/v3/src/views/CompareContainerView.js
@@ -12,13 +12,19 @@ CompareContainerView.prototype.render = function(){
 
 	var colors=this.utils.getDefaultColors();
 	var data = [];
+	var resultCount = 0;
 	this.model.forEach(function(t){
 		data.push({
 			label : t.label,
 			value : t.count,
 			color : colors.pop()
 		});
+		resultCount += t.count;
 	});
 	
 	this.utils.drawPie("arc", data, this.options.w/2, -this.options.h/2, 150, 120, 30, 0.4);
+
+	var gH = this.utils.getGroupByClassName('sp-group-label');
+	gH.attr('transform', xForm);
+	this.utils.addTextXForm(gH, this.options.w/2, -this.options.h, 'TOTAL SALES - ' + resultCount, 'sales-header');
 }
