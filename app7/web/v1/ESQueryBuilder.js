f01763cb36b2c50341196372101f54bd8d27b9b9
Vishal Sharma <vishal2033@gmail.com>
2015-07-03 10:07:09 +0530

    v1 work init on server side comparators

--- app7/web/v1/ESQueryBuilder.js
+++ app7/web/v1/ESQueryBuilder.js
@@ -1,7 +1,89 @@
 function ESQueryBuilder(){
+	this.client = new $.es.Client({
+	  hosts: 'localhost:9200'
+	});
+}
 
+ESQueryBuilder.prototype.getBasicQuery = function(){
+	return {
+		index: 'companysales',
+		type: 'sales',
+		body: {
+			query: {
+				match: {
+					category: 'Automobile'
+				}
+			},
+			//fields : ['city'],
+			size:25
+		}
+	}
+}
+
+ESQueryBuilder.prototype.getBasicFilteredQuery = function(){
+	return {
+		index: 'companysales',
+		type: 'sales',
+		body: {
+			query: {
+				filtered: {
+					query :{
+						match: {
+							category: 'Automobile'
+						}
+					},
+					filter : {
+						term : {'brand' : 'BMW'}
+					}
+
+				}		
+			},
+			size:25,
+			_source : true
+		}
+	}
+}
+
+ESQueryBuilder.prototype.getAndFilteredQuery = function(){
+	return {
+		index: 'companysales',
+		type: 'sales',
+		body: {
+			query: {
+				filtered: {
+					query :{
+						match: {
+							category: 'Automobile'
+						}
+					},
+					filter : {
+						and : [
+							{term : {'brand' : 'BMW'}},
+							{term : {'region': 'South'}},
+							{term : {'city' : 'Guntur'}}
+						]
+						
+					}
+
+				}		
+			},
+			size:25,
+			_source : true
+		}
+	}
 }
 
 ESQueryBuilder.prototype.executeQuery = function(url, cbOnDone){
-	$.getJSON(url, cbOnDone);
+	var esQuery = this.getBasicQuery();
+	this.client.search(esQuery).then(this.onQueryResponse.bind(this, cbOnDone), this.onQueryError.bind(this));	
+}
+
+ESQueryBuilder.prototype.onQueryResponse = function(cbOnDone, data){
+	cbOnDone({
+		success : true,
+		results : [data]
+	})
+}
+ESQueryBuilder.prototype.onQueryError = function(err){
+	console.trace(err.message);
 }
