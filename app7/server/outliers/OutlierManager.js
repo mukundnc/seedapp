579022b4567babe16eef926bd0046980c201bcab
Vishal Sharma <vishal2033@gmail.com>
2015-07-10 10:59:22 +0530

    outliers init

--- app7/server/outliers/OutlierManager.js
+++ app7/server/outliers/OutlierManager.js
@@ -34,7 +34,40 @@ OutlierManager.prototype.getOutliersForTop = function(resSearchQuery, line, cbOn
 	}
 	var respParser = new ResponseParser();
 	var searchResults = respParser.parse(resSearchQuery);
+	
+	if(line === 'product')
+		this.markOutliersInObject(searchResults[0]['key1']);
+	else
+		this.markOutliersInObject(searchResults[0]['key2']);
 	cbOnDone(searchResults);
+}
+
+OutlierManager.prototype.markOutliersInObject = function(obj){
+	var self = this;
+	obj.items.forEach(function(objChild){
+		if(objChild.items.length > 1){
+			var timeKeyVsItem = {};
+			var timeKeyVsCount = {};
+			objChild.items[1].items.forEach(function(objChildTimeItem){
+				timeKeyVsItem[objChildTimeItem.key] = objChildTimeItem;
+				timeKeyVsCount[objChildTimeItem.key] = objChildTimeItem.doc_count;
+			});
+			var timeKeyVsOutlierFlag = self.getOutlierFlagsForTimeItems(timeKeyVsCount);
+			Object.keys(timeKeyVsOutlierFlag).forEach(function(timeKey){
+				timeKeyVsItem[timeKey].outlier = timeKeyVsOutlierFlag[timeKey];
+			});
+		}
+	});
+}
+
+OutlierManager.prototype.getOutlierFlagsForTimeItems = function(timeKeyVsCount){
+	var i = 1;
+	var outlierFlags = {};
+	Object.keys(timeKeyVsCount).forEach(function(timeKey){
+		outlierFlags[timeKey] = i;
+		i++;
+	});
+	return outlierFlags;
 }
 
 module.exports = OutlierManager;
