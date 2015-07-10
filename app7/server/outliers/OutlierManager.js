7b8e87a6f18296b3a6ba2ac0c5570dcc6db9e25a
Vishal Sharma <vishal2033@gmail.com>
2015-07-10 21:23:03 +0530

    outlier drill down init

--- app7/server/outliers/OutlierManager.js
+++ app7/server/outliers/OutlierManager.js
@@ -1,5 +1,6 @@
 
 var OutlierTop = require('./OutlierTop');
+var ResponseParser = require('./../utils/ResponseParser');
 
 function OutlierManager(apiController){
 	this.apiController = apiController;
@@ -14,7 +15,9 @@ OutlierManager.prototype.handleOutlierRequest = function(reqHttp, resHttp){
 	var line = decodeURIComponent(reqHttp.query.line).toLowerCase();	
 
 	if(mode === 'top')
-		this.executeOutliersForTopMode(reqHttp, line, cbOnDone)
+		this.executeOutliersForTopMode(reqHttp, line, cbOnDone);
+	else if(mode === 'drilldown')
+		this.executeOutliersForDrilldownMode(reqHttp, query, line, cbOnDone);
 	
 }
 
@@ -28,5 +31,63 @@ OutlierManager.prototype.executeOutliersForTopMode = function(reqHttp, line, cbO
 	this.apiController.handleSearchRequest(reqHttp, onSearchQueryResponse);
 }
 
+OutlierManager.prototype.executeOutliersForDrilldownMode = function(reqHttp, query, line, cbOnDone){
+	var self = this;
+	var onSearchQueryResponse = {
+		json : function(res){
+			self.onSearchQueryResponse(res, query, line, cbOnDone);
+		}
+	}
+	this.apiController.handleSearchRequest(reqHttp, onSearchQueryResponse);
+}
+
+OutlierManager.prototype.onSearchQueryResponse = function(searchResults, query, line, cbOnDone){
+	var respParser = new ResponseParser();
+	var parsedResponse = respParser.parse(searchResults);
+	
+	if(!this.isDrilldownSupported(parsedResponse, line)){
+		cbOnDone({success : false, message : 'Operation not supported'});
+		return;
+	}
+	cbOnDone(parsedResponse);
+}
+
+OutlierManager.prototype.isDrilldownSupported = function(parsedResponse, line){
+	var products = ['categories', 'types', 'brands'];
+	var regions = ['regions'];
+	var supported = {
+		isProduct : false,
+		isRegion : false
+	};
+	var keys = ['key1', 'key2'];
+	keys.forEach(function(key){
+		var src = parsedResponse[0][key];
+		if(src){
+			if(products.indexOf(src.key) !== -1){
+				supported.isProduct = true;
+			}
+			if(regions.indexOf(src.key) !== -1){
+				supported.isRegion = true;
+			}
+		}
+	});
+	return line === 'product' ? supported.isProduct : supported.isRegion;
+}
+
+OutlierManager.prototype.getAllDrilldownSearches = function(parsedResponse, query, line){
+	var key = line === 'product' ? 'key1' : 'key2';
+	var drillDownItems = parsedResponse[0][key].items;
+	var drillDownSubjects = [];
+	drillDownItems.forEach(function(ddItem){
+		drillDownSubjects.push(ddItem.key);
+	});
+	var qSource = parsedResponse[0].queryDetails.qSource.value;
+
+	var drillDownQueries = [];
+	drillDownSubjects.forEach(function(ddSub){
+		drillDownQueries.push(query.replace(qSource, ddSub));
+	});
+	return drillDownQueries;
+}
 
 module.exports = OutlierManager;
