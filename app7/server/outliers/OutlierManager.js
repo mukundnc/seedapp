a6e64161870a031f0f9217441c5b5d78a0ca8d83
Vishal Sharma <vishal2033@gmail.com>
2015-07-10 10:13:06 +0530

    outliers init

--- app7/server/outliers/OutlierManager.js
+++ app7/server/outliers/OutlierManager.js
@@ -1,17 +1,40 @@
+var ResponseParser = require('./../utils/ResponseParser');
+
 function OutlierManager(apiController){
 	this.apiController = apiController;
 }
 
-OutlierManager.prototype.handleOutlierRequest = function(req, res){
-	var resp = {		
-		json : function(data){
-			res.json(data);
+OutlierManager.prototype.handleOutlierRequest = function(reqHttp, resHttp){
+	function cbOnDone(data){
+		resHttp.json(data);
+	}
+	var query = decodeURIComponent(reqHttp.query.q).toLowerCase();	
+	var mode = decodeURIComponent(reqHttp.query.mode).toLowerCase();	
+	var line = decodeURIComponent(reqHttp.query.line).toLowerCase();	
+
+	if(mode === 'top')
+		this.executeOutliersForTopMode(reqHttp, line, cbOnDone)
+	
+}
+
+OutlierManager.prototype.executeOutliersForTopMode = function(reqHttp, line, cbOnDone){
+	var self = this;
+	var onSearchQueryResponse = {
+		json : function(res){
+			self.getOutliersForTop(res, line, cbOnDone);
 		}
 	}
-	var query = decodeURIComponent(req.query.q).toLowerCase();	
-	var mode = decodeURIComponent(req.query.mode).toLowerCase();	
-	var line = decodeURIComponent(req.query.line).toLowerCase();		
-	this.apiController.handleSearchRequest(req, resp);
-}`
+	this.apiController.handleSearchRequest(reqHttp, onSearchQueryResponse);
+}
+
+OutlierManager.prototype.getOutliersForTop = function(resSearchQuery, line, cbOnDone){
+	if(!resSearchQuery.success){
+		cbOnDone({success : false, message : 'Internal error whlie executing search'});
+		return;
+	}
+	var respParser = new ResponseParser();
+	var searchResults = respParser.parse(resSearchQuery);
+	cbOnDone(searchResults);
+}
 
 module.exports = OutlierManager;
