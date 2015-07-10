3c1d90c9b5a041730b2a1032fd30198edfeae127
Vishal Sharma <vishal2033@gmail.com>
2015-07-10 09:38:25 +0530

    outliers init

--- app7/server/outliers/OutlierManager.js
+++ app7/server/outliers/OutlierManager.js
@@ -8,8 +8,10 @@ OutlierManager.prototype.handleOutlierRequest = function(req, res){
 			res.json(data);
 		}
 	}
-
+	var query = decodeURIComponent(req.query.q).toLowerCase();	
+	var mode = decodeURIComponent(req.query.mode).toLowerCase();	
+	var line = decodeURIComponent(req.query.line).toLowerCase();		
 	this.apiController.handleSearchRequest(req, resp);
-}
+}`
 
 module.exports = OutlierManager;
