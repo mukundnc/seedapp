dc609e869d7633cfabac4a186132b3ae313f6fe4
Vishal Sharma <vishal2033@gmail.com>
2015-08-02 12:26:02 +0530

    app8 v2 UI init with BS

--- app8/web/v2/app.js
+++ app8/web/v2/app.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
 })
 
 function setViewPort(){	
-	// $('.body-container').css('height' , window.innerHeight + 'px');
-	// var svg = d3.selectAll('svg');
-	// svg.attr('width', $('.svg-container').width());
-	// svg.attr('height', $('.svg-container').height());
+	$('.body-container').css('height' , (window.innerHeight+200) + 'px');
+	var svg = d3.selectAll('svg');
+	svg.attr('width', $('.svg-container').width());
+	svg.attr('height', $('.svg-container').height());
 	$('.opt-menu-icon').css('top', -($('.svg-container').height()) + 'px')
 }
