597abdf91487ac907c77cb29e9fabcb96702399c
Vishal Sharma <vishal2033@gmail.com>
2015-08-02 17:48:18 +0530

    add BS to app7

--- app7/web/v3/app.js
+++ app7/web/v3/app.js
@@ -8,9 +8,10 @@ $(document).ready(function(){
 })
 
 function setViewPort(){	
-	$('.body-container').css('height' , window.innerHeight + 'px');
-	var svg = d3.selectAll('svg');
-	svg.attr('width', $('.svg-container').width());
-	svg.attr('height', $('.svg-container').height());
+	var availableHeight = window.innerHeight - $('.searchbar-container').height();
+	$('.body-container').css('height' , (availableHeight+200) + 'px');
+	var svgView = d3.selectAll('.svg-view');
+	svgView.attr('width', $('.svg-container').width());
+	svgView.attr('height', availableHeight);
 	$('.opt-menu-icon').css('top', -($('.svg-container').height()) + 'px')
 }
